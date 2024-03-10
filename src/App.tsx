a1930082609f9e0fad3a19d575fe63350c4e1976
ushankv2005 <ushanbala2005@gmail.com>
2024-03-10 14:17:24 +0530

    Added signin and sign up

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Login from './Login';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.min.css';
@@ -279,7 +279,6 @@ const Services = () => (
 );
 const ClinicApp = () => (
   <>
-
     <Topbar />
     <Navbar />
     <Header />
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ClinicApp from './App.tsx'
 import './index.css'
+import Login from './Login.tsx'
+import Register from './Register.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClinicApp />
+    <Login />
   </React.StrictMode>,
 )
